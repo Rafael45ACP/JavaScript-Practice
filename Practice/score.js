14bfa786bbb60f00c4245c4b5ef7518c6816babb
Rafael45ACP <rafaelgriego1911@gmail.com>
2026-03-19 01:55:59 +0000

    forgot this haha

---
+++ Practice/score.js
@@ -0,0 +1,14 @@
+let score = 80;
+
+if(score < 50){
+    console.log("Fail")
+}
+else if(score >= 50 && score <= 74){
+    console.log("Pass")
+}
+else if(score >= 75 && score <=89){
+    console.log("Good")
+}
+else if(score >= 90){
+    console.log("Excellent")
+}
