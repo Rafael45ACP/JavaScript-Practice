434b269bd059079c33c953b4e6c27a0c1122011d
Rafael45ACP <rafaelgriego1911@gmail.com>
2026-03-19 01:55:10 +0000

    up to level 2

---
+++ Practice/num1.js
@@ -0,0 +1,12 @@
+let num1 = 4; 
+let num2 = 3;
+
+if(num1 === num2){
+    console.log("Equal")
+}
+else if(num1 > num2){
+    console.log("Num1 is bigger")
+}
+else{
+    console.log("Num2 is bigger")
+}
---
+++ Practice/temp.js
@@ -0,0 +1,8 @@
+let temperature = 332;
+
+if(temperature > 30){
+    console.log("Hot")
+}
+else{
+    console.log("Not Hot")
+}
